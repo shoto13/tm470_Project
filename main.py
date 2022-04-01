1fd8bfa267ad182ab9c5569f2060e0d7d6bc8513
shoto13 <wilsonrc@pm.me>
2022-04-01 23:03:03 +0100

    starting to set up webdriver options

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 #TM470 Project
 
-print('Hello world');
+from selenium import webdriver
+from tkinter import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import time
+
+# BROWSER SETUP
+chrome_options = webdriver.ChromeOptions()
+PATH = "/home/vxv/chromedriver"
+driver = webdriver.Chrome(
+    executable_path=PATH,
+    chrome_options=chrome_options
+)
+
+driver.get('https://www.youtube.com')
